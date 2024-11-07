c5c2eaa5fd6e0ca8c957d6c0bd707bada60637d5
Daniel Divinus <creativecodex65@gmail.com>
2024-11-06 21:30:55 -0600

    Modified game inventory

--- game_inventory.py
+++ game_inventory.py
@@ -1,4 +1,5 @@
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+orc_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def display_inventory(inv):
   total_inv = 0
@@ -6,6 +7,16 @@ def display_inventory(inv):
   for k, v in inv.items():
     print(f'\t{v} {k}')
     total_inv += v
+  print()
   print(f'\tTotal number of items: {total_inv}')
 
+def add_to_inv(inv, loot):
+  for k in loot:
+    if k in inv:
+      inv[k] += 1
+    else:
+      inv[k] = 1
+
+add_to_inv(inventory, orc_loot)
 display_inventory(inventory)
+
