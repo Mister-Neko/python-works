ba4f25d9a46240553828bdc27f166e914a5ea9c2
Daniel Divinus <creativecodex65@gmail.com>
2024-11-07 11:16:30 -0600

    Modified mclip.py

--- mclip.py
+++ mclip.py
@@ -2,6 +2,8 @@
 # mclip.py - multi-clipboard program. v0.0.0
 
 from sys import argv
+from pyperclip import copy as pcopy
+from pyperclip import paste
 
 TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
         'busy': """Sorry, can we do this later this week or next week?""",
@@ -10,3 +12,11 @@ TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
 if len(argv) < 2:
   print('error: no arguments provided')
   exit()
+
+key_phrase = argv[1]
+
+if key_phrase in TEXT:
+  pcopy(TEXT[key_phrase])
+  print(f'Text for {key_phrase} copied to keyboard')
+else:
+  print(f'There is no text for {key_phrase}.')
