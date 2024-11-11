2edda0bff47feac61e5932a019f51e26b34eef06
Daniel Divinus <creativecodex65@gmail.com>
2024-11-11 12:17:46 -0600

    Modified phone_email_copy.py

--- phone_email_copy.py
+++ phone_email_copy.py
@@ -13,7 +13,7 @@ r_phone = re.compile(r'''
   ''', re.X)
 
 r_email = re.compile(r'''
-  (\w{1,64})         # Email Prefix
+  ([\w._%+-]{1,64})         # Email Prefix
   ([@].{1,252})      # Separator and Domain
   ([.]\w{3})         # Top-level Domain
   ''', re.X)
@@ -21,15 +21,18 @@ r_email = re.compile(r'''
 def j_found_str(txt):
   temp = ''
   max = len(txt)
-  for i in range(max):
-    try:
-      if i != max - 1:
-        temp += ''.join(txt[i]) + '\n'
-      else:
-        temp += ''.join(txt[i])
-    except IndexError:
-      return None
-  return temp
+  if max > 0:
+    for i in range(max):
+      try:
+        if i != max - 1:
+          temp += ''.join(txt[i]) + '\n'
+        else:
+          temp += ''.join(txt[i])
+      except IndexError:
+        return None
+    return temp
+  else:
+    return None
 
 txt = paste()
 phone_num = j_found_str(r_phone.findall(txt))
@@ -38,27 +41,24 @@ email = j_found_str(r_email.findall(txt))
 if phone_num != None and email != None:
   txt = (f'Phone Number:\n{phone_num}\nEmail:\n{email}')
   pcopy(txt)
+  print(paste())
 elif phone_num != None:
-  txt = (f'Phone Number: {phone_num}')
+  txt = (f'Phone Number:\n{phone_num}')
   pcopy(txt)
+  print(paste())
 elif email != None:
-  txt = (f'Email: {email}')
+  txt = (f'Email:\n{email}')
   pcopy(txt)
-
-print(paste())
+  print(paste())
+else:
+  print('No phone numbers or emails found.')
 
 '''
-Anne_Gibbons8910@zynuu.tech
-Caleb_Evans4631@xqj6f.property
-Joseph_Watt6556@ckzyi.store
-Mya_Lunt2514@c2nyu.auction
-Adalind_Henderson8008@voylg.business
-
-418-543-8090
-587-530-2271
-404-724-1937
-443-307-1473
-329-420-1792
-770-212-6011
-473-522-7496
+info@nostarch.com
+info@nostarch.com
+800-420-7240
+415-863-9900
+media@nostarch.com
+415-863-9950
+academic@nostarch.com
 '''
