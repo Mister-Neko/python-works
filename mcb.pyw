ffb32b9017685e41a33ce216a581f7f0b96a726f
Daniel Divinus <creativecodex65@gmail.com>
2024-11-22 19:15:22 -0600

    Modified new_py.py and created mcb.pyw

---
+++ mcb.pyw
@@ -0,0 +1,2 @@
+#! python
+# mcb.pyw - Saves and loads pieces of text to the clipboard. - v0.0.0
--- new_py.py
+++ new_py.py
@@ -1,15 +1,23 @@
 #! python
 # new_py.py - Quickly create new Python files. - v0.0.0
+from pyinputplus import inputInt
 
 def new_py():
-  print('What is the name of the program: ', end='')
+  print('What is the name of the program: ')
   new_file_name = input()
+ 
+  file_ext = inputInt(prompt=f'Select file extension\n{' ' * 4}[1] .py\n{' ' * 4}[2] .pyw\n', blockRegexes=['[03-9]'])
+  if file_ext == 1:
+    ext = '.py'
+  elif file_ext == 2:
+    ext = '.pyw'
+ 
   print('Describe what the program will do: ')
   program_des = input()
   new_py_txt = f'''#! python
-# {new_file_name}.py - {program_des} - v0.0.0
+# {new_file_name}{ext} - {program_des} - v0.0.0
 '''
-  f_obj = open(f'{new_file_name}.py', 'w')
+  f_obj = open(f'{new_file_name}{ext}', 'w')
   f_obj.write(f'{new_py_txt}')
   f_obj.close()
 
